99abdb2d80acade907b23a793e2460694dc6bfec
meetabug <1272526724@qq.com>
2019-05-09 17:37:25 +0800

    翻译队列化

---
+++ app/Jobs/TranslateSlug.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Handlers\SlugTranslateHandler;
+use App\Models\Topic;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TranslateSlug implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $topic;
+
+    public function __construct(Topic $topic)
+    {
+        $this->topic =  $topic;
+    }
+
+
+    public function handle()
+    {
+        $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
+
+        \DB::table('topics')->where('id',$this->topic->id)->update(['slug' => $slug]);
+    }
+}

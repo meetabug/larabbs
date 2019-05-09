e86e96df16b50f898a06f5c9a54611973885c9d5
meetabug <1272526724@qq.com>
2019-05-09 11:36:19 +0800

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Http\Requests\TopicRequest;
 use App\Models\Topic;
+use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
@@ -12,9 +14,9 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request,Topic $topic)
 	{
-		$topics = Topic::with('user','category')->paginate(30);
+        $topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 

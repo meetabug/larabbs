e3c006b1858c2379a71772f5a72ecfc7ebff19f6
meetabug <1272526724@qq.com>
2019-05-09 14:30:14 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,7 @@ Route::resource('users','UsersController',['only' => ['show','update','edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 //分类
-
 Route::resource('categories','CategoriesController',['only'=>['show']]);
+
+//图片上传
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');

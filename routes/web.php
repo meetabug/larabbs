3a46820c00a8e207da8db6c4ffe17d89e4a3f362
meetabug <1272526724@qq.com>
2019-05-10 15:12:41 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 //消息通知列表
 Route::resource('notifications','NotificationsController',['only' => ['index']]);
+
+//无权限
+Route::get('permission-denied','PagesController@permissionDenied')->name('permission-denied');
